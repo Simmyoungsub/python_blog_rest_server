428ea92aa502be7ca25d8872ea5d92acba8289ea
myoungsubsim <rop2001@gmail.com>
2017-09-24 20:37:20 +0900

    설정 파일 수정

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -69,8 +69,8 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
-        'rest_framework_social_oauth2.authentication.SocialAuthentication',
+        #'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        #'rest_framework_social_oauth2.authentication.SocialAuthentication',
         # 'blog.util.UserService'
     ),
 }
