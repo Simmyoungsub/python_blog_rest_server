05d12c9243787d913616f751db1d2b6f669ecab0
myoungsubsim <rop2001@gmail.com>
2017-09-24 20:14:31 +0900

    설정 파일 수정

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -134,7 +134,7 @@ CORS_ORIGIN_WHITELIST = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase'
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
